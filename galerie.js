6dcde2eb95638e4471add10975a4dbc364d03417
yattou <yanis.attou@mmibordeaux.com>
2026-04-02 16:57:57 +0200

    update gallery

--- galerie.js
+++ galerie.js
@@ -1,11 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const track = document.querySelector(".galerie-caroussel");
-const items = document.querySelectorAll(".galerie-photo");
+const tracks = document.querySelectorAll(".galerie-caroussel");
 
-const getScrollAmount = () => track.scrollWidth - window.innerWidth;
+const getScrollAmount = () => tracks[0].scrollWidth - window.innerWidth;
 
-const tween = gsap.to(track, {
+const tween = gsap.to(tracks, {
     x: () => -getScrollAmount(),
     ease: "none"
 });
@@ -18,21 +17,4 @@ ScrollTrigger.create({
     animation: tween,
     scrub: 1,
     invalidateOnRefresh: true
-});
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            items.forEach(item => item.classList.remove('is-active'));
-            entry.target.classList.add('is-active');
-        }
-    });
-}, {
-    root: null,
-    rootMargin: "0px -50% 0px -50%",
-    threshold: 0
-});
-
-items.forEach(item => {
-    observer.observe(item);
 });
